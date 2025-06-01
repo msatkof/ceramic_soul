7f08e65b783647e5e4f884f13192ebcb3b47af92
Mihail <shatkovskij98@gmail.com>
2025-06-01 18:13:54 +0300

    adding the init commit

---
+++ src/js/script.js
@@ -0,0 +1,41 @@
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
+
+import "purecss/build/grids-min.css";
+import "purecss/build/grids-responsive-min.css";
+
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+import "../sass/style.scss";
+
+try {
+    new Swiper('.works__slider', {
+        slidesPerView: 1,
+        loop: true,
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+        breakpoints: {
+            // when window width is >= 1200px
+            1200: {
+                slidesPerView: 3,
+                spaceBetween: 5,
+            },
+
+            1920: {
+                spaceBetween: 35,
+            }
+
+        },
+
+        modules: [Navigation, Pagination],
+
+    });
+} catch (e) { }
