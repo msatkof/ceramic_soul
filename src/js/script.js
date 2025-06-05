9389420aae90332f5e3c4914854bb777f4e6062d
Mihail <shatkovskij98@gmail.com>
2025-06-05 21:47:26 +0300

    added birger animation and menu for mobile

--- src/js/script.js
+++ src/js/script.js
@@ -10,6 +10,20 @@ import 'swiper/css/pagination';
 
 import "../sass/style.scss";
 
+const burger = document.querySelector(".burger"),
+    close = document.querySelector(".header__menu-close"),
+    menu = document.querySelector(".header__menu");
+
+burger.addEventListener("click", () => {
+    menu.classList.add("header__menu_active");
+    document.body.style.overflow = "hidden";
+});
+
+close.addEventListener("click", () => {
+    menu.classList.remove("header__menu_active");
+    document.body.style.overflow = "";
+});
+
 try {
     new Swiper('.works__slider', {
         slidesPerView: 1,
